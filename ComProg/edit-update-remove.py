e9fe1b8b3438e547c579686ca8cad05fa8c668bc
Fran <86468924+Fwanu@users.noreply.github.com>
2024-03-10 22:04:52 +0800

    Create edit-update-remove.py

---
+++ ComProg/edit-update-remove.py
@@ -0,0 +1,76 @@
+# Online Python compiler (interpreter) to run Python online.
+# Write Python 3 code in this online editor and run it.
+
+arr = []
+pop = 0
+i = 1
+
+while i != 0:
+    
+    
+    info = input("Input information here one by one, then press e if done: ")
+    arr.append(info)
+    print(arr)
+    
+    
+    if info == "e":
+        arr.pop(-1)
+        print()
+        print("\n {0}".format(arr))
+        
+        cloop = 1
+        
+        while cloop == 1:    
+        
+            try:
+                choice = int(input("""1 - edit
+2 - update
+3 - remove
+4 - end
+Pick a choice: """))
+                break
+    
+            except ValueError:
+                print("That's not a number bruv")
+                continue
+            
+            if choice > 5:
+                print("That's not a choice mate")
+                continue
+            
+        if choice == 1:
+            
+            lop1 = True
+            
+            while lop1 == True:
+                print("\n{0}".format(arr))
+                arrcount = len(arr)
+            
+                bleh = int(input("Insert number of sequence you wanna edit: "))
+                
+                
+                if bleh > arrcount:
+                    print("Bruh, that's called adding")
+                    
+                elif bleh < 0:
+                    print("I don't hold negative indexes moron")
+                    
+                else:
+                    replace = input("What do you want to replace this data with?: ")
+                    
+                    arr[bleh-1] = replace
+                    print(arr)
+                    
+                    continue
+                
+        if choice == 2:
+            
+            lop2 = True
+                
+            
+            
+                
+        
+        
+    
+    
